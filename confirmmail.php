5ced4e9bef1d808e1bf3dd98ca084a4935f0c6df
Filippo Valle <filippo.valle@edu.unito.it>
2017-09-16 15:59:09 +0200

    default

--- confirmmail.php
+++ confirmmail.php
@@ -1,18 +1,18 @@
 <?php
-
+// using SendGrid's PHP Library
+// https://github.com/sendgrid/sendgrid-php
+// If you are using Composer (recommended)
 require 'vendor/autoload.php';
-
-$apiKey="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";
-$from = new SendGrid\Email("Glifico", "info@glifico.com");
-$subject = "Account created on Glifico";
-$dest=$_GET["to"];
-$to = new SendGrid\Email("Translator", "fili007@me.com");
-$content = new SendGrid\Content("text/html", "Thank you for registrating on <b>Glifico</b>");
+// If you are not using Composer
+// require("path/to/sendgrid-php/sendgrid-php.php");
+$from = new SendGrid\Email("Example User", "test@example.com");
+$subject = "Sending with SendGrid is Fun";
+$to = new SendGrid\Email("Example User", "test@example.com");
+$content = new SendGrid\Content("text/plain", "and easy to do anywhere, even with PHP");
 $mail = new SendGrid\Mail($from, $subject, $to, $content);
+$apiKey = getenv('SENDGRID_API_KEY');
 $sg = new \SendGrid($apiKey);
 $response = $sg->client->mail()->send()->post($mail);
 echo $response->statusCode();
 print_r($response->headers());
 echo $response->body();
-
-?>
