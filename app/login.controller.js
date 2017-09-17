0a94e3001701917a3dc7f5b574b2164372249419
Filippo Valle <fvalle1@users.noreply.github.com>
2017-09-17 08:48:02 +0200

    $ convention

--- app/login.controller.js
+++ app/login.controller.js
@@ -5,20 +5,19 @@ angular.module('Authentication',[])
 	ctrl.username=getUsername();
 	
 	
-	function getUsername(){
-		var return_value = null;
+	function getUsername() {
+    		var return_value = null;
 
-	    var pos_start = $document[0].cookie.indexOf("=");
+    		var pos_start = document.cookie.indexOf(cookie_name+"=");
 
-	    if (pos_start != -1) { // Cookie already set, read it
-	    	pos_start++;
-	        var pos_end = $document[0].cookie.indexOf(";", pos_start); // Find ";" after the start position
+    		if (pos_start != -1) { // Cookie already set, read it
+    			pos_start++;
+			var pos_end=document.cookie.indexOf("$", pos_start); // Find ";" after the start position
+        		if (pos_end == -1) pos_end = document.cookie.length;
+        		return_value = unescape( document.cookie.substring(pos_start, pos_end) );
+   		 }
 
-	        if (pos_end == -1) pos_end = document.cookie.length;
-	        return_value = unescape( $document[0].cookie.substring(pos_start, pos_end) );
-	    }
-
-	    return return_value; // null if cookie doesn't exist, string otherwise
+   	 return return_value; // null if cookie doesn't exist, string otherwise
 	}
 });
 
