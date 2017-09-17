a577d316a8ed97b28d56eb2a06f5bddc6781aa58
Filippo Valle <filippo.valle@edu.unito.it>
2017-09-17 23:27:47 +0200

    upload cleared

--- app/upload.controller.js
+++ app/upload.controller.js
@@ -2,34 +2,54 @@
  * 
  */
 
-angular.module("upController",[])
-.controller("uploadController",function uploadController(){
+angular.module("uploadController",[])
+.controller("uploadController",function upController(){
 	var ctrl=this;
-	
-	var client = filestack.init('ALwn7aYCDTAConyTqVba8z');
-	
-	ctrl.$init=function(){
-		ctrl.showpay=false;
-		ctrl.storedUrl="";
-	};
-	
+
+	var client = filestack.init('ALwn7aYCDTAConyTqVba8z',
+			{
+		policy:"eyJoYW5kbGUiOiJBTHduN2FZQ0RUQUNvbnlUcVZiYTh6IiwiZXhwaXJ5IjoxNTA1NjgyMzE5fQ",
+			});
+
+	ctrl.$onInit=function(){
+		ctrl.storedUrl="http://www.example.com";
+		ctrl.uploaded=false;
+		document.getElementById("remove").style.visibility="hidden";
+	}
+
+
 	ctrl.showPicker=function() {
 		client.pick({
 			accept: ['.pdf','.odt','.doc','.docx','.txt'],
 			maxFiles: 1,
+			onClose(){
+				console.debug("close");
+			},
+			onFileUploadFinished(){
+				ctrl.storedUrl="endend";
+				console.debug("finished");
+			},
 		}).then(function(result) {
-			ctrl.storedUrl=result.filesUploaded[0]["url"];
-			console.log(ctrl.storedUrl);
-			ctrl.showpay=true;
+			document.getElementById("url").innerHTML="<a href=\""+result.filesUploaded[0]["url"]+"\" target=\"_Blank\">Download File</a>";
+			document.getElementById("remove").style.visibility="visible";
+			document.getElementById("upDescr").style.visibility="hidden";
+		},function(result){
+			alert("error while uploading");
 		});
+
+	};
+
+	ctrl.remove=function(){
+		document.getElementById("url").innerHTML="";
+		document.getElementById("upThanks").style.visibility="hidden";
 	};
 
 })
 
 
 angular.element(document).ready(function() {
-        console.log("registro upload");
-        angular.bootstrap(document.getElementById('upController'), ['upController']);
-    });
+	console.log("registro upload");
+	angular.bootstrap(document.getElementById('upController'), ['uploadController']);
+});
 
 
