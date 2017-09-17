b3949b88620de48820b5bb8a30576ae7bdfea9ce
Filippo Valle <fvalle1@users.noreply.github.com>
2017-09-17 08:28:42 +0200

    using regex correctly

--- functions.js
+++ functions.js
@@ -72,8 +72,7 @@ function saveTheCookie(value) {
 function getLogged(){
 	var storedText = document.cookie;
 	console.debug("getlogged");
-	console.debug(/maincookie=[a-zA-Z0-9]{4,}[;\s]{0,1}/.test(storedText));
-	return /maincookie=[a-zA-Z0-9]{4,}[;\s]/g.test(storedText); 
+	return /maincookie=[a-zA-Z0-9]{4,}[;\s]{0,1}/g.test(storedText); 
 }
 
 
