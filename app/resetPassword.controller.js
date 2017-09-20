1b034f12e90813db47916018940f0934f2350018
Filippo Valle <filippo.valle@edu.unito.it>
2017-09-20 14:19:51 +0200

    remove debug

--- app/resetPassword.controller.js
+++ app/resetPassword.controller.js
@@ -31,13 +31,13 @@ angular.module("resetController",[])
 	ctrl.onChangePassword=function(){
 		var req=createXHTMLHttpRequest();
 		req.onreadystatechange=function(){
-			if(req.status==200){
+			if(req.status==200&&req.ready==4){
 				if(JSON.parse(req.responseText)['statuscode']==200){
 					ctrl.resetCompleted=true;
 					$("#form").hide();
 					message('Thanks. Password updated');
 				}else{
-					errorMessage("wrong token");
+					errorMessage("Wrong token");
 				}
 			}else{
 				errorMessage("There was an error, please retry");
@@ -52,15 +52,13 @@ angular.module("resetController",[])
 
 
 	function message(messageStr){
-		$('#alertOK').fadeIn().delay(200).fadeOut;
+		$('#alertOK').fadeIn().delay(2000).fadeOut;
 		$('#alertOK').html(messageStr);
 	};
 
 	function errorMessage(messageStr){
-		$('#alertError').fadeIn().delay(100)
-		.fadeOut();
-		$('#alertError')
-		.html(messageStr);
+		$('#alertError').html(messageStr);
+		$('#alertError').fadeIn().delay(1000).fadeOut();
 	}
 });
 
