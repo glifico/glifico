e22c49712eef0ce70c9e17ac8438c10f8571c256
Filippo Valle <filippo.valle@edu.unito.it>
2017-09-21 10:46:59 +0200

    remove html tag

--- confirmMailT.php
+++ confirmMailT.php
@@ -29,7 +29,7 @@ $data='{
   "content": [
     {
       "type": "text/plain",
-      "value": "To complete registration on <b>Glifico</b> open or copy this link in a browser:'.$link.'"
+      "value": "To complete registration on Glifico open or copy this link in a browser:'.$link.'"
     }
   ]
 }';
