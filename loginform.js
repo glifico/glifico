148fb508251ea501184797a7ab3ba4d74b332bc8
Filippo Valle <filippo.valle@edu.unito.it>
2017-09-23 08:50:19 +0200

    debug

--- loginform.js
+++ loginform.js
@@ -200,6 +200,8 @@
 		}];
 
 		vmMainController_regTrad.submit = function() {
+			console.debug(grecaptcha.getResponse());
+			console.dubug(grecaptcha.getResponse().length);
 			if(grecaptcha.getResponse().length>1){
 				var req = createXHTMLHttpRequest() ;
 				req.onreadystatechange = function(){
