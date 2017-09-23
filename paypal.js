c75e5d1293f2a50ae085545bb60ab6985b3a3bd0
Filippo Valle <filippo.valle@edu.unito.it>
2017-09-23 07:46:31 +0200

    change account

--- paypal.js
+++ paypal.js
@@ -1,9 +1,9 @@
 $(document).ready(function () {
 	$('#payCompleted').hide();
-	
+
 	if(!getLogged()){
 		$("#styledbody").hide();
-		
+
 		alert("Please login");
 	}
 });
@@ -23,19 +23,19 @@ paypal.Button.render({
 	env: 'sandbox', // Or 'production'
 
 	locale: 'en_US',
-	
+
 	client: {
-		sandbox:    'AZDxjDScFpQtjWTOUtWKbyN_bDt4OgqaF4eYXlewfBP4-8aqX3PiV8e1GWU6liB2CUXlkA59kJXE7M6R',
+		sandbox:    'AZRBS2QBR8wGqZxOApvDTn-Mte7wfv7iXGLxa-tN1UlmvFflKoyulHT_eunIcuwdW7667-jSgOOgCU5V',
 		production: '<????>'
 	},
 
 	commit: true, // Show a 'Pay Now' button
 
 	style: {
-		size:'medium',	
+		size:'medium',
 		color: 'silver',
 	},
-	
+
 	payment: function(data, actions) {
 		return actions.payment.create({
 			payment: {
@@ -52,21 +52,21 @@ paypal.Button.render({
             }
 		});
 	},
-	
-	
+
+
 
 	onAuthorize: function(data, actions) {
 		return actions.payment.execute().then(function(payment) {
 			// The payment is complete!
 			// You can now show a confirmation message to the customer
-			
+
 			alert("good payment");
-			
+
 			paymentCompleted();
-			
+
 		});
 	},
-	
+
 	onCancel: function(data, actions) {
         alert("Payment doesn't end well :-(");
     }
@@ -79,7 +79,7 @@ paypal.Button.render({
 angular.module("payment",[])
 .controller("paymentController",function(){
 	var ctrl=this;
-	
+
 	ctrl.job="Test translation";
 	ctrl.translator="John Appleseed";
 	ctrl.amount="10";
@@ -91,7 +91,3 @@ angular.element(document).ready(function() {
     console.log("registro payment");
     angular.bootstrap(document.getElementById('payment'), ['payment']);
 });
-
-
-
-
