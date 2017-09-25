3054e5ad00112c5aa3e11c5efe10d907bcbb0087
Filippo Valle <filippo.valle@edu.unito.it>
2017-09-25 14:37:08 +0200

    introduce Guassian admin

---
+++ gaussianAdmin/gaussian.controller.js
@@ -0,0 +1,44 @@
+angular.module("gaussian",[])
+.controller("gaussianController",function(){
+  var ctrl=this;
+
+  var classes=[
+    {
+      "size": 10,
+      "margin": 50,
+      "fix": 0
+    },
+    {
+      "size": 60,
+      "margin": 20,
+      "fix": 0
+    },
+    {
+      "size": 30,
+      "margin": 50,
+      "fix": 10
+    }
+  ];
+
+  ctrl.$onInit=function(){
+    ctrl.data=classes;
+    ctrl.sizeOk=true;
+    ctrl.checkStyle="";
+  }
+
+  ctrl.formChange=function(){
+    console.log("form changed");
+    var sum=0;
+    for (var i=0; i<ctrl.data.length; i++) {
+      sum+=parseInt(ctrl.data[i]['size']);
+    }
+    ctrl.sizeOk = (sum==100);
+    if(sum!=100){
+      ctrl.checkStyle="background:red";
+    }else{
+      ctrl.checkStyle="";
+    }
+  }
+
+
+})
