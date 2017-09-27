6717d9d2e56aca71b5450dcd2305fb2dfea929be
Filippo Valle <filippo.valle@edu.unito.it>
2017-09-27 07:34:01 +0200

    implement storeTest

--- saveTest.php
+++ saveTest.php
@@ -1,4 +1,21 @@
 <?php
 
-echo($_POST['test']);
+function search($id){
+  return 3;
+}
+
+$user=$_POST['user'];
+$document=json_decode($_POST['document'],true);
+$domande=$document['domande'];
+
+$score=0;
+
+foreach ($domande as $domanda) {
+  $id=$domanda['id'];
+  $risposta=search($id);
+  if($risposta==$domanda['scelta']) $score+=1;
+  echo($score);
+}
+
+exit($score);
  ?>
