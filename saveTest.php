af4c721bb005df8c9eeecd6ffc8dc4449c592e03
Filippo Valle <filippo.valle@edu.unito.it>
2017-09-28 12:31:32 +0200

    calculate score

--- saveTest.php
+++ saveTest.php
@@ -1,8 +1,27 @@
 <?php
 
-function search($id){
-  return 3;
+function search($db, $id){
+  $query="SELECT id, scelta WHERE id='$id'";
+
+  $result = $db->query($query);
+  $row = $result->fetch(PDO::FETCH_ASSOC);
+
+  $result->CloseCursor();
+  return $row['scelta'];
 }
+
+$dbopts = parse_url(getenv('DATABASE_URL'));
+$dsn = "pgsql:"
+."host=".$dbopts["host"].";"
+. "dbname=".ltrim($dbopts["path"],'/').";"
+. "user=".$dbopts["user"].";"
+. "port=5432;"
+. "sslmode=require;"
+. "password=".$dbopts["pass"];
+
+
+$db = new PDO($dsn);
+if(!$db) exit();
 
 $user=$_POST['user'];
 $document=json_decode($_POST['document'],true);
@@ -12,10 +31,10 @@ $score=0;
 
 foreach ($domande as $domanda) {
   $id=$domanda['id'];
-  $risposta=search($id);
+  $risposta=search($db, $id);
   if($risposta==$domanda['scelta']) $score+=1;
-  echo($score);
+  $score+=1;
 }
 
 exit(json_encode(array("message"=>"Test submitted","statuscode"=>400,"score"=>$score)));
- ?>
+?>
