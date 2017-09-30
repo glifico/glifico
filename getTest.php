af2d8cbd138b333a78332e8f03a2e24f3818521b
Filippo Valle <filippo.valle@edu.unito.it>
2017-09-30 11:17:31 +0200

    order by test

--- getTest.php
+++ getTest.php
@@ -32,7 +32,7 @@ if(!$db) exit;
 $user=$_GET['user'];
 if(!certToken($db, $user,$_GET['token'])) exit(json_encode(array("message"=>"wrong token", "statuscode"=>400)));
 
-$query="SELECT username, language, datatest, tottest FROM languages WHERE username='$user' ORDER BY language;";
+$query="SELECT username, language, datatest, tottest FROM languages WHERE username='$user' ORDER BY tottest DESC;";
 $result = $db->query($query);
 $toExit=[];
 while($row = $result->fetch(PDO::FETCH_ASSOC)){
