87ba88abf375c9c913d4a73071a323f46eb7eacc
Filippo Valle <filippo.valle@edu.unito.it>
2017-10-06 07:47:25 +0200

    add id

--- getAgencyData.php
+++ getAgencyData.php
@@ -29,5 +29,5 @@ $result = $db->query($query);
 $row = $result->fetch(PDO::FETCH_ASSOC);
 
 
-exit (json_encode([array("CompanyName"=>$row['nome'], "FiscalCode"=>$row['vat'], "VATCode"=>$row['vat'], "Street"=>$row['street'], "Number"=>$row['number'], "Number"=>$row['number'], "City"=>$row['citta'],"StateProvince"=>$row['provincia'],"ZIP"=>$row['cap'], "Country"=>$row['stato'], "EmailReference"=>$row['email'],"Bank"=>$row['banca'], "PayamentMode"=>$row['pagamento'], "IBAN"=>$row['iban'],"EmailReferenceBilling"=>$row['email'])]));
+exit (json_encode([array("id"=>$row['id'], "CompanyName"=>$row['nome'], "FiscalCode"=>$row['vat'], "VATCode"=>$row['vat'], "Street"=>$row['street'], "Number"=>$row['number'], "Number"=>$row['number'], "City"=>$row['citta'],"StateProvince"=>$row['provincia'],"ZIP"=>$row['cap'], "Country"=>$row['stato'], "EmailReference"=>$row['email'],"Bank"=>$row['banca'], "PayamentMode"=>$row['pagamento'], "IBAN"=>$row['iban'],"EmailReferenceBilling"=>$row['email'])]));
 ?>
