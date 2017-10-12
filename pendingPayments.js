876378956d16d73368968d69891058896f2a7b6e
Filippo Valle <filippo.valle@edu.unito.it>
2017-10-12 09:46:56 +0200

    fix apex

--- pendingPayments.js
+++ pendingPayments.js
@@ -45,12 +45,11 @@ angular.module("pendingPayments",[]).controller("pendingPayments",function(){
 				html+='<button onClick="newPayment('+doc.id+')"  class="btn btn-primary">Pay now!</button>';				
 			}else if(doc.status=="Ongoing"){
 				html+='<button type="button" class="btn btn-info" data-toggle="modal" data-target="#jobModal"';
-				html+='data-job='+doc.job;
-				html+=' data-price='+doc.price;
-				html+=' data-currency='+doc.currency;
-				console.log(doc.description);
-				html+=' data-description="'+doc.description;
-				html+='">Show job</button>';
+				html+='data-job="'+doc.job+'"';
+				html+=' data-price="'+doc.price+'"';
+				html+=' data-currency="'+doc.currency+'"';
+				html+=' data-description="'+doc.description+'"';
+				html+='>Show job</button>';
 			}
 			html+='</td>';
 			html+='<tr>';
