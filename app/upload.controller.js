6bf0ef6a017b52f62096f05b2e0d6ffdfe2dbe35
Filippo Valle <filippo.valle@edu.unito.it>
2017-10-12 20:02:17 +0200

    clean

--- app/upload.controller.js
+++ app/upload.controller.js
@@ -11,9 +11,6 @@ angular.module("uploadController",[])
 	});
 
 	ctrl.$onInit=function(){
-		ctrl.title=ctrl.getTitle();
-		ctrl.storedUrl="http://www.example.com";
-		ctrl.uploaded=false;
 		ctrl.translator=getUsername();
 		//document.getElementById("remove").style.visibility="hidden";
 	}
@@ -24,43 +21,19 @@ angular.module("uploadController",[])
 			accept: ['.pdf','.odt','.doc','.docx','.txt'],
 			maxFiles: 1,
 			onClose(){
-				console.debug("close");
+
 			},
 			onFileUploadFinished(){
-				ctrl.storedUrl="endend";
-				console.debug("finished");
+
 			},
 		}).then(function(result) {
-			document.getElementById("url").innerHTML="<a href=\""+result.filesUploaded[0]["url"]+"\" target=\"_Blank\">Download File</a>";
-			document.getElementById("remove").style.visibility="visible";
-			document.getElementById("upDescr").style.visibility="hidden";
+
 		},function(result){
 			alert("error while uploading");
 		});
 
 	};
 
-	ctrl.remove=function(){
-		document.getElementById("url").innerHTML="";
-		document.getElementById("upDescr").style.visibility="visible";
-		document.getElementById("remove").style.visibility="hidden";
-	};
-
-	
-	ctrl.getTitle=function(){
-	var start=location.href.indexOf("token=");
-	start+=6;
-	var end=location.href.length;
-	var tok=location.href.substring(start,end);
-	
-	var toReturn=ctrl.askServer(tok);
-	
-	return toReturn;
-	};
-	
-	ctrl.askServer=function(tok){
-		return "Titolo da richiedere al server con il token"+tok;
-	};
 	
 })
 
