bd4790ab3f4582c2a50063fbc86106b6a19f0629
Filippo Valle <filippo.valle@edu.unito.it>
2017-10-12 09:07:53 +0200

    fix

--- getPayment.php
+++ getPayment.php
@@ -1,4 +1,21 @@
 <?php
-exit(json_encode(array("id"=>$_GET['id'],"job"=>"pippo","price"=>14,"currency"=>"€")));
+include 'functions.php';
 
- ?>
+$db=getDB();
+if(!$db) exit;
+
+$user=$_GET['user'];
+$id=$_GET['id'];
+if(!certTokenA($db, $user,$_GET['token'])) exit(json_encode(array("message"=>"wrong token", "statuscode"=>400)));
+
+$query="SELECT * FROM payments WHERE username='$user' and id='$id' LIMIT 1;";
+$result = $db->query($query);
+$toExit=[];
+$row = $result->fetch(PDO::FETCH_ASSOC);
+array_push($toExit,array("id"=>$row['id'],"job"=>$row['job'],"price"=>$row['price'],"currency"=>$row['currency'],"status"=>$row['status'],"description"=>$row['description']));
+
+
+$result->CloseCursor();
+
+exit(json_encode($toExit));
+?>
