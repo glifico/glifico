413e3a171b208f7aea84dab5b0ce2ef63e95cb8d
Filippo Valle <filippo.valle@edu.unito.it>
2017-10-22 11:30:55 +0200

    dummy price

--- getTranslators.php
+++ getTranslators.php
@@ -12,7 +12,7 @@ $query="SELECT * FROM traduttore LIMIT 10;";
 $result = $db->query($query);
 $toExit=[];
 while($row = $result->fetch(PDO::FETCH_ASSOC)){
-  array_push($toExit,array("FirstName"=>$row['nome']{0},"LastName"=>$row['cognome']{0}, "IdCountry"=>$row['idstato'],"IdMothertongue"=>$row['madrelingua']));
+  array_push($toExit,array("Price"=>2, "FirstName"=>$row['nome']{0},"LastName"=>$row['cognome']{0}, "IdCountry"=>$row['idstato'],"IdMothertongue"=>$row['madrelingua']));
 }
 
 
