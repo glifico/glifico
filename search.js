4e39eb2b34b368be031e09ab0c9830a2ffdf8b7e
Filippo Valle <filippo.valle@edu.unito.it>
2017-10-22 11:20:11 +0200

    firsts implementation of search

--- search.js
+++ search.js
@@ -2,48 +2,29 @@ $(document).ready(function () {
 
 });
 
-// 	$.ajax( {
-// 		type : "POST",
-// 		dataType : "application/json",
-// 		contentType : "application/json; charset=utf-8",
-// 		data : data,
-// 		url : "setJobOngoing.php",
-// 		complete : function(ret) {
-// 			var response=ret.responseText;
-// 			$("#alertOK").html("Job finished!");
-// 			$("#alertOK").fadeIn().delay(2000).fadeOut();
-// 			$("#jobModal").hide();
-// 			location.href=location.href;
-// 		},
-// 		error : function(xhr) {
-// 			if (xhr.status == 500) {
-// 				$("#alertError").html("Error from server, please retry.");
-// 				$("#alertError").fadeIn().delay(1000).fadeOut();
-// 			}
-//
-// 		}
-// 	});
-//
-// }
+//$.ajax( {
+//type : "POST",
+//dataType : "application/json",
+//contentType : "application/json; charset=utf-8",
+//data : data,
+//url : "setJobOngoing.php",
+//complete : function(ret) {
+//var response=ret.responseText;
+//$("#alertOK").html("Job finished!");
+//$("#alertOK").fadeIn().delay(2000).fadeOut();
+//$("#jobModal").hide();
+//location.href=location.href;
+//},
+//error : function(xhr) {
+//if (xhr.status == 500) {
+//$("#alertError").html("Error from server, please retry.");
+//$("#alertError").fadeIn().delay(1000).fadeOut();
+//}
 
-var $star_rating = $('.star-rating .fa');
+//}
+//});
 
-var SetRatingStar = function() {
-  return $star_rating.each(function() {
-    if (parseInt($star_rating.siblings('input.rating-value').val()) >= parseInt($(this).data('rating'))) {
-      return $(this).removeClass('fa-star-o').addClass('fa-star');
-    } else {
-      return $(this).removeClass('fa-star').addClass('fa-star-o');
-    }
-  });
-};
-
-$star_rating.on('click', function() {
-  $star_rating.siblings('input.rating-value').val($(this).data('rating'));
-  return SetRatingStar();
-});
-
-SetRatingStar();
+//}
 
 
 angular.module("search",[]).controller("search",function(){
@@ -62,7 +43,7 @@ angular.module("search",[]).controller("search",function(){
 			"job":"ppp",
 			"price":"oooo"
 		}
-	]
+		]
 
 	ctrl.createTable=function(){
 		var html="";
@@ -70,48 +51,38 @@ angular.module("search",[]).controller("search",function(){
 		html+='<thead class="thead-default">';
 		html+='<tr class="row">';
 		html+='<th class="col-md-4" style="text-align:center;">Translator</th>';
+		html+='<th class="col-md-4" style="text-align:center;">From</th>';
+		html+='<th class="col-md-4" style="text-align:center;">To</th>';
+		html+='<th class="col-md-4" style="text-align:center;">Field</th>';
+		html+='<th class="col-md-4" style="text-align:center;">Rating</th>';
 		html+='<th class="col-md-4" style="text-align:center;">Price</th>';
-		html+='<th class="col-md-4" style="text-align:center;">Value</th>';
 		html+='<th class="col-md-4" style="text-align:center;"></th>';
 		html+='</tr>';
 		html+='</thead>';
 		for (var i = 0; i < ctrl.documents.length; i++) {
 			var doc=ctrl.documents[i];
 			html+='<tr class="row '+ctrl.getClass(doc)+'">';
-			html+='<td class="col-md-4">'+doc.job+'</td>';
-			html+='<td class="col-md-4">'+doc.price+doc.currency+'</td>';
-			if(doc.status=="Pending"){
-				html+='<td class="col-md-4" data-toggle="tooltip" data-placement="top" title="'+ctrl.getToolTip(doc)+'">'+"Accepted"+'</td>';
-			}else{
-				html+='<td class="col-md-4" data-toggle="tooltip" data-placement="top" title="'+ctrl.getToolTip(doc)+'">'+doc.status+'</td>';
-			}
+			html+='<td class="col-md-4">'+doc.FirstName+doc.LastName+'</td>';
+			html+='<td class="col-md-4">'+doc.IdMothertongue+'</td>';
+			html+='<td class="col-md-4">'+doc.IdMothertongue+'</td>';
+			html+='<td class="col-md-4">'+doc.Field+'</td>';
+			html+='<td class="col-md-4">'+doc.Rating+'</td>';
 			html+='<td class="col-md-4">';
-			html+='<div id="'+doc.id+'">';
-			if(doc.status=="Completed"){
-				html+='<i class="fa fa-check" aria-hidden="true"></i></div>';
-			}else if(doc.status=="Ongoing"){
-				html+='<button type="button" class="btn btn-warning" data-toggle="modal" data-target="#jobModal"';
-				html+='data-job="'+doc.job+'"';
-				html+=' data-price="'+doc.price+'"';
-				html+=' data-id="'+doc.id+'"';
-				html+=' data-currency="'+doc.currency+'"';
-				html+=' data-status="'+doc.status+'"';
-				html+=' data-description="'+doc.description+'"';
-				html+='>Show job</button>';
-			}else if(doc.status=="Finished"){
-				html+='Waiting approval...';
-			}else if(doc.status=="Pending"){
-				html+='Waiting payment...';
-			}else if(doc.status=="Proposed"){
-				html+='<button type="button" class="btn btn-danger" data-toggle="modal" data-target="#jobModal"';
-				html+='data-job="'+doc.job+'"';
-				html+=' data-price="'+doc.price+'"';
-				html+=' data-id="'+doc.id+'"';
-				html+=' data-currency="'+doc.currency+'"';
-				html+=' data-status="'+doc.status+'"';
-				html+=' data-description="'+doc.description+'"';
-				html+='>Show job and accept it</button>';
+			html+='<div class="rating">';
+			switch(doc.Price){
+			case 3:
+				var class1="filled";
+			case 2:
+				var class2="filled";
+			case 1:
+				var class3="filled";
+			default:
+				break;
 			}
+			html+='<span class="money '+class1+'"><i class="fa fa-usd fa-3x" aria-hidden="true" data-rating="1" onclick="alert("you pushed 1")"></i></span>';
+			html+='<span class="money '+class2+'"><i class="fa fa-usd fa-3x" aria-hidden="true" data-rating="2" onclick="alert("you pushed 2")"></i></span>';
+			html+='<span class="money '+class3+'"><i class="fa fa-usd fa-3x" aria-hidden="true" data-rating="3" onclick="alert("you pushed 3")"></i></span>';
+			html+='</div>';
 			html+='</td>';
 			html+='<tr>';
 		}
@@ -120,24 +91,22 @@ angular.module("search",[]).controller("search",function(){
 	}
 
 	ctrl.$onInit=function(){
-		ctrl.createTable();
-		// 	var url = "getPendingJobs.php?user=" + getUsername()+"&token="+getToken();
-		//
-		// 	var req = createXHTMLHttpRequest() ;
-		// 	req.onreadystatechange = function(){
-		// 		if (req.status == 200&req.readyState==4){
-		// 			var data=JSON.parse(req.responseText);
-		// 			ctrl.documents=data;
-		// 			ctrl.createTable();
-		// 			return(true);
-		// 		}else{
-		// 			mostraDialogTimed('errorPanel');
-		// 			return(false);
-		// 		}
-		// 	}
-		// 	req.open("GET",url,true);
-		// 	req.send();
-		// }
+		var url = "getTranslators.php?user=" + getUsername()+"&token="+getToken();
+
+		var req = createXHTMLHttpRequest();
+		req.onreadystatechange = function(){
+			if (req.status == 200&req.readyState==4){
+				var data=JSON.parse(req.responseText);
+				ctrl.documents=data;
+				ctrl.createTable();
+				return(true);
+			}else{
+				mostraDialogTimed('errorPanel');
+				return(false);
+			}
+		}
+		req.open("GET",url,true);
+		req.send();
 	}
 });
 
