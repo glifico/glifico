0ed26f8f11ec06e5de7f793fa32c7eda83c26996
Filippo Valle <filippo.valle@edu.unito.it>
2017-10-24 19:36:17 +0200

    fix

--- getTranslators.php
+++ getTranslators.php
@@ -25,7 +25,7 @@ if(!certTokenA($db, $user, $token)) exit(json_encode(array("message"=>"wrong tok
 $query="SELECT * FROM languages WHERE language='$langTo' AND tottest>='$reqRating';";
 $result = $db->query($query);
 
-$pricequery="SELECT avg(price) FROM(SELECT price FROM traduttore) sub;";
+$pricequery="SELECT avg(price) FROM($query) sub;";
 $priceRes=$db->query($query);
 //$price=$priceRes->fetch(PDO::FETCH_ASSOC);
 $price=2;
@@ -34,8 +34,11 @@ $price=2;
 $toExit=[];
 while($row = $result->fetch(PDO::FETCH_ASSOC)){
   $translator=$row['username'];
+  $queryUser="SELECT * FROM traduttore WHERE username='$translator';";
+  $resultUser = $db->query($queryUser);
+  $rowUser=$resultUser->fetch(PDO::FETCH_ASSOC);
   $rating=$row['tottest'];
-  array_push($toExit,array("Price"=>$price, "Rating"=>$rating, "FirstName"=>$row['nome']{0},"LastName"=>$row['cognome']{0}, "IdCountry"=>$row['idstato'],"IdMothertongue"=>$row['madrelinguaid'],"Mothertongue"=>$row['madrelingua']));
+  array_push($toExit,array("Price"=>$price, "Rating"=>$rating, "FirstName"=>$rowUser['nome']{0},"LastName"=>$rowUser['cognome']{0}, "IdMothertongue"=>$rowUser['madrelinguaid'],"Mothertongue"=>$rowUser['madrelingua']));
 }
 
 $result->CloseCursor();
