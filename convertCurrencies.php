78e9b1bc928afa75600c949c1512473591ea794a
Filippo Valle <filippo.valle@edu.unito.it>
2017-10-26 22:15:46 +0200

    round

--- convertCurrencies.php
+++ convertCurrencies.php
@@ -18,7 +18,7 @@ while($row = $result->fetch(PDO::FETCH_ASSOC)){
   $from=$row['from_l'];
   $price=$row['price'];
   $cur=substr($row['currency'],0,3);
-  $priceEuro=truncate((float)$price/$handle['rates'][$cur],2);
+  $priceEuro=round((float)$price/$handle['rates'][$cur],2);
   $query="UPDATE language_pair SET price_euro='$priceEuro' WHERE username='$username' AND from_l='$from' AND to_l='$to' AND price='$price';";
   $db->query($query);
 }
