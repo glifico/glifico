3f301ba3ef58175a3435322ce10dc10b86446e01
Filippo Valle <filippo.valle@edu.unito.it>
2017-10-26 07:49:52 +0200

    introduce get Pairs

--- getLanguagePairsData.php
+++ getLanguagePairsData.php
@@ -1,25 +1,20 @@
 <?php
 include 'functions.php';
 
-// $dbopts = parse_url(getenv('DATABASE_URL'));
-// $dsn = "pgsql:"
-// ."host=".$dbopts["host"].";"
-// . "dbname=".ltrim($dbopts["path"],'/').";"
-// . "user=".$dbopts["user"].";"
-// . "port=5432;"
-// . "sslmode=require;"
-// . "password=".$dbopts["pass"];
-// $db = new PDO($dsn);
-// if(!$db) exit;
-//
-//
-// $user=$_GET['user'];
-// if(!certToken($db, $user, $_GET['token'])) exit(json_encode(array("message"=>"wrong token", "statuscode"=>400)));
+$db=getDB();
+if(!$db) exit;
 
-// $query="SELECT * FROM traduttore WHERE username='$user';";
-// $result = $db->query($query);
-// $row = $result->fetch(PDO::FETCH_ASSOC);
+$user=$_GET['user'];
+if(!certToken($db, $user, $_GET['token'])) exit(json_encode(array("message"=>"wrong token", "statuscode"=>400)));
 
-exit(json_encode(array("statuscode"=>200)));
-exit (json_encode([array("FirstName"=>$row['nome'],"LastName"=>$row['cognome'],"Birthday"=>$row['data_nascita'],"City"=>$row['citta'],"StateProvince"=>$row['provincia'],"ZIP"=>$row['cap'], "IdCountry"=>$row['idstato'],"IdMothertongue"=>$row['madrelingua'])]));
+$query="SELECT username, from_l, to_l, price from language_pair WHERE username='$user';";
+$result = $db->query($query);
+
+$toExit=[];
+while($row = $result->fetch(PDO::FETCH_ASSOC)){
+
+  array_push($toExit,array("LanguageFrom"=>$row['from_l'], "LanguageTo"=>$row['to_l'], "Price"=>$row['price']);
+}
+
+exit (json_encode($toExit));
 ?>
