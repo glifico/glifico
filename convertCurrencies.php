ab9f2bcf01bc9f2cd3ce43e7a97100d349c0c0c9
Filippo Valle <filippo.valle@edu.unito.it>
2017-10-26 22:02:00 +0200

    you shoul divide instead of multiply

--- convertCurrencies.php
+++ convertCurrencies.php
@@ -18,11 +18,7 @@ while($row = $result->fetch(PDO::FETCH_ASSOC)){
   $from=$row['from_l'];
   $price=$row['price'];
   $cur=substr($row['currency'],0,3);
-  $priceEuro=$price*$handle['rates'][$cur];
-  echo($price);
-  echo($priceEuro);
-  echo($handle['rates'][$cur]);
-  echo("</br>");
+  $priceEuro=$price/$handle['rates'][$cur];
   $query="UPDATE language_pair SET price_euro='$priceEuro' WHERE username='$username' AND from_l='$from' AND to_l='$to' AND price='$price';";
   $db->query($query);
 }
