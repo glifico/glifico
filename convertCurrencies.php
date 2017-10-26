4ac99e69b59c26fb0d7c1161435f2bcadcc52633
Filippo Valle <filippo.valle@edu.unito.it>
2017-10-26 21:39:54 +0200

    fix

--- convertCurrencies.php
+++ convertCurrencies.php
@@ -11,6 +11,8 @@ $url="https://v3.exchangerate-api.com/bulk/72aabbf884e8a2247df4bdff/EUR";
 //$url="https://api.fixer.io/latest";
 $handle = file_get_contents($url);
 
+echo($handle);
+
 $toExit=[];
 while($row = $result->fetch(PDO::FETCH_ASSOC)){
   $username=$row['username'];
@@ -18,11 +20,15 @@ while($row = $result->fetch(PDO::FETCH_ASSOC)){
   $from=$row['from_l'];
   $price=$row['price'];
   $cur=substr($row['currency'],0,3);
+  echo($cur);
   $priceEuro=$price*$handle['rates'][$cur];
+echo($priceEuro);
   $query="UPDATE language_pair SET price_euro='$priceEuro' WHERE username='$username' AND from_l='$from' AND to_l='$to' AND price='$price';";
+$echo($query);
   $result = $db->query($query);
 }
 
+$result->CloseCursor();
 exit(json_encode(array("stauscode"=>200,"message"=>"currencies updated")));
 
 ?>
