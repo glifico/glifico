5d3ea1d657570370bb000905290d1f2a2a1eaa8a
Filippo Valle <filippo.valle@edu.unito.it>
2017-10-27 22:56:13 +0200

    add "

--- search.js
+++ search.js
@@ -105,7 +105,7 @@ angular.module("search",[]).controller("search",function($scope){
 				html+='<td class="col-md-2">';
 				html+='<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#TrModal"';
 				html+=' data-id="'+doc.id+'"';
-				html+=' ng-click="ctrl.calculatePriceAg('+doc.Price+','+doc.PriceTr+')';
+				html+=' ng-click="ctrl.calculatePriceAg('+doc.Price+','+doc.PriceTr+')"';
 				html+='>Select translator</button>';
 				html+='</td>';
 				html+='<tr>';
