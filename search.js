335eea7078a2e1185507858d6ce5f64aa4d3ae64
Filippo Valle <filippo.valle@edu.unito.it>
2017-10-27 23:06:10 +0200

    remove 1 from days to let count tomorrow as 1

--- search.js
+++ search.js
@@ -215,7 +215,7 @@ angular.module("search",[]).controller("search",function($scope){
 
 	ctrl.calcultateFeasibility=function(){
 		var timeDiff = Math.abs(ctrl.TrDeadline.getTime() - ctrl.today.getTime());
-		var days = Math.ceil(timeDiff / (1000 * 3600 * 24));
+		var days = Math.ceil(timeDiff / (1000 * 3600 * 24))-1;
 		ctrl.MaxCh=days*10000;
 		ctrl.UrgCh=days*8500;
 		ctrl.days=days;
