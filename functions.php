d7aa5c5a92460808ce5698cb7e1f52117198b5d0
Filippo Valle <filippo.valle@edu.unito.it>
2017-11-12 14:08:37 +0100

    use email as function

--- functions.php
+++ functions.php
@@ -49,4 +49,37 @@ function get_user_email($user){
   return $email;
 }
 
+function send_email($to, $subject, $body){
+  $url="https://api.sendgrid.com/v3/mail/send";
+  $handle = curl_init($url);
+  $data='{
+    "personalizations": [
+      {
+        "to": '.json_encode($to).',
+        "subject": "'.$subject.'"
+      }
+    ],
+    "from": {
+      "email": "info@glifico.com"
+    },
+    "content": [
+      {
+        "type": "text/plain",
+        "value": "'.$body.'"
+      }
+    ]
+  }';
+
+  curl_setopt($handle, CURLOPT_RETURNTRANSFER, true);
+  curl_setopt($handle, CURLOPT_CONNECTTIMEOUT, 5);
+  curl_setopt($handle, CURLOPT_TIMEOUT, 60);
+  curl_setopt($handle, CURLOPT_HTTPHEADER, array(
+    'Authorization: Bearer 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',
+    'Content-Type: application/json'
+  ));
+  curl_setopt($handle,CURLOPT_POSTFIELDS, $data);
+  $result = curl_exec($handle);
+
+}
+
  ?>
