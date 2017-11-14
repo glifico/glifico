7e8a4b36702821fb868634374a1d15839cfccd80
Filippo Valle <filippo.valle@edu.unito.it>
2017-11-14 07:49:15 +0100

    fix typo R

--- getLanguagePairsData.php
+++ getLanguagePairsData.php
@@ -7,13 +7,13 @@ if(!$db) exit;
 $user=$_GET['user'];
 if(!certToken($db, $user, $_GET['token'])) exit(json_encode(array("message"=>"wrong token", "statuscode"=>400)));
 
-$query="SELECT username, from_l, to_l, price from language_pair WHERE username='$user';";
+$query="SELECT username, from_l, to_l, price, currency from language_pair WHERE username='$user';";
 $result = $db->query($query);
 
 $toExit=[];
 while($row = $result->fetch(PDO::FETCH_ASSOC)){
 
-  array_push($toExit,array("LanguageFrom"=>$row['from_l'], "LanguageTo"=>$row['to_l'], "Price"=>$row['price'], "Currency"=>$Row['currency']));
+  array_push($toExit,array("LanguageFrom"=>$row['from_l'], "LanguageTo"=>$row['to_l'], "Price"=>$row['price'], "Currency"=>$row['currency']));
 }
 
 exit (json_encode($toExit));
