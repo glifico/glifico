afca6c855ff37db70907fdd65281c9e9f42ba092
Filippo Valle <filippo.valle@edu.unito.it>
2017-11-27 07:35:40 +0100

    add field

--- getLanguagePairsData.php
+++ getLanguagePairsData.php
@@ -7,13 +7,12 @@ if(!$db) exit;
 $user=$_GET['user'];
 if(!certToken($db, $user, $_GET['token'])) exit(json_encode(array("message"=>"wrong token", "statuscode"=>400)));
 
-$query="SELECT username, from_l, to_l, price, currency from language_pair WHERE username='$user';";
+$query="SELECT username, from_l, to_l, price, currency, field, service from language_pair WHERE username='$user';";
 $result = $db->query($query);
 
 $toExit=[];
 while($row = $result->fetch(PDO::FETCH_ASSOC)){
-
-  array_push($toExit,array("LanguageFrom"=>$row['from_l'], "LanguageTo"=>$row['to_l'], "Price"=>$row['price'], "Currency"=>$row['currency']));
+  array_push($toExit,array("LanguageFrom"=>$row['from_l'], "LanguageTo"=>$row['to_l'], "Price"=>$row['price'], "Currency"=>$row['currency'],"Field"=>$row['field'],"Service"=>$row['service']));
 }
 
 exit (json_encode($toExit));
