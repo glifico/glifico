aabb64a437949289f61eaed45ae241fc590d2699
Filippo Valle <filippo.valle@edu.unito.it>
2017-11-29 20:38:09 +0100

    first time can delete

--- translator.js
+++ translator.js
@@ -592,7 +592,7 @@ function strLeft(sourceStr, keyStr) {
 				html+='<tr class="row">';
 				html+='<td class=" col-md-5"><span style="font-size:18px;">From: '+edu.LanguageFrom+', To:'+ edu.LanguageTo +', Price: '+edu.Price+' '+ edu.Currency+'</span></td>';
 				html+='<td class=" col-md-5"><button   data-toggle="modal" data-target="#LanguageModal"  data-price="'+edu.Price+'" class="md-secondary md-hue-3" aria-label="edit"><span><i class="fa fa-pencil fa-2x"></i></span></button></td>';
-				html+='<td class=" col-md-5"><button ng-click="doDelete=('+edu.Id+')"><span aria-label="delete" class="md-secondary md-hue-3" ><i class="fa fa-trash fa-2x"></i></span></button></td>';
+				html+='<td class=" col-md-5"><button ng-click="doDelete=('+edu.IdLanguageFrom+','+edu.IdLanguageTo+')"><span aria-label="delete" class="md-secondary md-hue-3" ><i class="fa fa-trash fa-2x"></i></span></button></td>';
 				html+='</tr>';
 			}
 			html+='</thead>';
@@ -654,7 +654,10 @@ function strLeft(sourceStr, keyStr) {
 		var arr={
 				"user": getUsername(),
 				"token": getToken(),
-				"values": $scope.model,
+				"values": {
+					"IdLanguageFrom":from,
+					"IdLanguageTo":to,
+				}
 		};
 
 		var stringPass=JSON.stringify(arr);
