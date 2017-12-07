62044fb3ceab0529bcdd5975f9a276fd32ee6cc3
Filippo Valle <filippo.valle@edu.unito.it>
2017-12-07 18:20:01 +0100

    test not working admin page

--- admin/gaussian.controller.js
+++ admin/gaussian.controller.js
@@ -1,44 +1,57 @@
-angular.module("gaussian",[])
-.controller("gaussianController",function(){
-  var ctrl=this;
+angular.module('admin', [])
+.controller('AdminCtrl', function AdminCtrl() {
 
-  var classes=[
-    {
-      "size": 10,
-      "margin": 50,
-      "fix": 0
-    },
-    {
-      "size": 60,
-      "margin": 20,
-      "fix": 0
-    },
-    {
-      "size": 30,
-      "margin": 50,
-      "fix": 10
-    }
-  ];
+});
 
-  ctrl.$onInit=function(){
-    ctrl.data=classes;
-    ctrl.sizeOk=true;
-    ctrl.checkStyle="";
+
+
+angular.module('admin').component('gaussianComponent', {
+  template: '<span>Name: {{$ctrl.hero.name}}</span>',
+  bindings: {
+    hero: '='
   }
-
-  ctrl.formChange=function(){
-    console.log("form changed");
-    var sum=0;
-    for (var i=0; i<ctrl.data.length; i++) {
-      sum+=parseInt(ctrl.data[i]['size']);
-    }
-    ctrl.sizeOk = (sum==100);
-    if(sum!=100){
-      ctrl.checkStyle="background:red";
-    }else{
-      ctrl.checkStyle="";
-    }
-  }
+});
 
 
-})
+
+/**
+var classes=[
+{
+"size": 10,
+"margin": 50,
+"fix": 0
+},
+{
+"size": 60,
+"margin": 20,
+"fix": 0
+},
+{
+"size": 30,
+"margin": 50,
+"fix": 10
+}
+];
+
+ctrl.$onInit=function(){
+ctrl.data=classes;
+ctrl.sizeOk=true;
+ctrl.checkStyle="";
+}
+
+ctrl.formChange=function(){
+console.log("form changed");
+var sum=0;
+for (var i=0; i<ctrl.data.length; i++) {
+sum+=parseInt(ctrl.data[i]['size']);
+}
+ctrl.sizeOk = (sum==100);
+if(sum!=100){
+ctrl.checkStyle="background:red";
+}else{
+ctrl.checkStyle="";
+}
+}
+
+},
+**/
