c2a31993bbb5478564f16cfe790a63d367e96d07
Filippo Valle <filippo.valle@edu.unito.it>
2017-12-19 20:29:13 +0100

    introduce gaussian admin

--- admin/gaussian.controller.js
+++ admin/gaussian.controller.js
@@ -1,57 +1,37 @@
 angular.module('admin', [])
 .controller('AdminCtrl', function AdminCtrl() {
+  var ctrl=this;
 
+  var classes=[
+    {
+      "size": 10,
+      "margin": 50,
+      "fix": 0
+    },
+    {
+      "size": 60,
+      "margin": 20,
+      "fix": 0
+    },
+    {
+      "size": 30,
+      "margin": 50,
+      "fix": 10
+    }
+  ]
+
+  ctrl.$onInit=function(){
+    ctrl.data=classes;
+    ctrl.checkStyle="";
+  }
+  
 });
 
 
 
-angular.module('admin').component('gaussianComponent', {
-  template: '<span>Name: {{$ctrl.hero.name}}</span>',
+angular.module('admin').component('gaussiancomponent', {
+  templateUrl: 'app/glificoGaussian.template.html',
   bindings: {
     hero: '='
   }
 });
-
-
-
-/**
-var classes=[
-{
-"size": 10,
-"margin": 50,
-"fix": 0
-},
-{
-"size": 60,
-"margin": 20,
-"fix": 0
-},
-{
-"size": 30,
-"margin": 50,
-"fix": 10
-}
-];
-
-ctrl.$onInit=function(){
-ctrl.data=classes;
-ctrl.sizeOk=true;
-ctrl.checkStyle="";
-}
-
-ctrl.formChange=function(){
-console.log("form changed");
-var sum=0;
-for (var i=0; i<ctrl.data.length; i++) {
-sum+=parseInt(ctrl.data[i]['size']);
-}
-ctrl.sizeOk = (sum==100);
-if(sum!=100){
-ctrl.checkStyle="background:red";
-}else{
-ctrl.checkStyle="";
-}
-}
-
-},
-**/
