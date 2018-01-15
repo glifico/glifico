90d58564b660a8537be87d54816363b6d1d00271
Filippo Valle <filippo.valle@edu.unito.it>
2018-01-15 07:47:08 +0100

    add key

--- satispaycreatecharge.php
+++ satispaycreatecharge.php
@@ -12,7 +12,7 @@ if(!$data){
 
 $uid=$data['uuid']
 
-\SatispayOnline\Api::setSecurityBearer('osh_...');
+\SatispayOnline\Api::setSecurityBearer('osh_ea7knn45ljmjon41kbq542jdl7112k928aeddpvlb9o76qh0o6kfe2md7fh3taufj8l67kaforua611clg0b9e1ss90tl073b770l6jpmk4gjp0evvbui4rrdn6ggr2kcpj13nqn36ht88mmoqsujk1q02ojbsqai3klf0s7aqsdd195aa8bin4kvr7vc9ta6cvc8fcg');
 \SatispayOnline\Api::setSandbox(true);
 
 $charge = \SatispayOnline\Charge::create([
