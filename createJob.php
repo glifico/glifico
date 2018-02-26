c957978e424df776b0136df71aa11e25c95ad80d
Filippo Valle <filippo.valle@edu.unito.it>
2018-02-26 15:43:58 +0100

    send email to correct address

--- createJob.php
+++ createJob.php
@@ -30,8 +30,8 @@ $result = $db->query($query);
 
 $result->CloseCursor();
 
-//send_email([array("email"=>get_user_email($translator))],"There is a new job on glifico","You have a new job on glifico, got to https://glifico.com/pendingJobs.html to look it out!");
-send_email([array("email"=>"fvalle.glifico@outlook.com")],"There is a new job on glifico",get_user_email($translator));
+send_email([array("email"=>get_user_email($translator))],"There is a new job on glifico","You have a new job on glifico, got to https://glifico.com/pendingJobs.html to look it out!");
+//send_email([array("email"=>"fvalle.glifico@outlook.com")],"There is a new job on glifico",get_user_email($translator));
 
 exit(json_encode(array("message"=>"job created", "statuscode"=>200,"selected"=>json_encode($selected))));
 ?>
