408c2fc4c20caa921cd99ad5d3ddcd49b4cefb2c
Filippo Valle <filippo.valle@edu.unito.it>
2018-03-12 08:09:57 +0100

    change echo

--- getRatingQuestions.php
+++ getRatingQuestions.php
@@ -8,11 +8,8 @@ $user=$_GET['user'];
 if(!certToken($db, $user,$_GET['token'])) exit(json_encode(array("message"=>"wrong token", "statuscode"=>400)));
 
 $lang=$_GET['lang'];
-echo($lang);
 $query="SELECT id, language, text_to_translate, topic FROM ratingTest WHERE language='$lang' ORDER BY RANDOM() LIMIT 1;";
-echo($query);
 $result = $db->query($query);
-echo("kk");
 $row = $result->fetch(PDO::FETCH_ASSOC);
 echo($row);
 echo("aa");
