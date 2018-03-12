5515236b7e95b0a29a3685af6f839e9141b2a544
Filippo Valle <filippo.valle@edu.unito.it>
2018-03-12 10:04:15 +0100

    do not return text

--- getRatingQuestions.php
+++ getRatingQuestions.php
@@ -8,7 +8,7 @@ $user=$_GET['user'];
 if(!certToken($db, $user,$_GET['token'])) exit(json_encode(array("message"=>"wrong token", "statuscode"=>400)));
 
 $lang=$_GET['lang'];
-$query="SELECT id, language, text_to_translate, topic FROM ratingTest WHERE language='$lang' ORDER BY RANDOM() LIMIT 1;";
+$query="SELECT id, language, topic FROM ratingTest WHERE language='$lang' ORDER BY RANDOM() LIMIT 1;";
 $result = $db->query($query);
 $row = $result->fetch(PDO::FETCH_ASSOC);
 echo($row);
