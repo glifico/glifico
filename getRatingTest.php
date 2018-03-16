c4044e973954defca469a6b6683fef69de9c49e2
Filippo Valle <filippo.valle@edu.unito.it>
2018-03-16 07:34:05 +0100

    introduce rating table

---
+++ getRatingTest.php
@@ -0,0 +1,20 @@
+<?php
+include 'functions.php';
+
+$db=getDB();
+if(!$db) exit;
+
+$user=$_GET['user'];
+if(!certToken($db, $user,$_GET['token'])) exit(json_encode(array("message"=>"wrong token", "statuscode"=>400)));
+
+$query="select * from languagerating WHERE username='$user';";
+$result = $db->query($query);
+$toExit=[];
+while($row = $result->fetch(PDO::FETCH_ASSOC)){
+  array_push($toExit,array("Language"=>$row['language'], "DataTest"=>$row['datatest']));
+}
+
+$result->CloseCursor();
+
+exit(json_encode($toExit));
+?>
--- socialTrad.js
+++ socialTrad.js
@@ -31,35 +31,35 @@ function init() {
 			return(false);
 		}
 	}
-	
+
 	req.open("GET", url, true);
 	req.send();
 }
 
 function gotLanguages(data){
-		// Combo box 1
-			var html = "";
-			html += '<select id="select-language" class="form-control">'
-			html += '<option value="-"></option>'
+	// Combo box 1
+	var html = "";
+	html += '<select id="select-language" class="form-control">'
+		html += '<option value="-"></option>'
 
 			for (i = 0; i < data.length; i++) {
 				html += '<option value="' + data[i].IdLanguageTo + '">'
-						+ data[i].LanguageTo + '</option>'
+				+ data[i].LanguageTo + '</option>'
 			}
-			html += '</select>'
-			// Combo box 2
-			var html2 = "";
-			html2 += '<select id="select-language2" class="form-control">'
-			html2 += '<option value="-"></option>'
+	html += '</select>'
+		// Combo box 2
+		var html2 = "";
+	html2 += '<select id="select-language2" class="form-control">'
+		html2 += '<option value="-"></option>'
 
 			for (i = 0; i < data.length; i++) {
 				html2 += '<option name="' + data[i].Language + '" value="'
-						+ data[i].IdLanguageTo + '">' + data[i].LanguageTo
-						+ '</option>'
+				+ data[i].IdLanguageTo + '">' + data[i].LanguageTo
+				+ '</option>'
 			}
-			html2 += '</select>'
-			$('#span-combo-lingue').html(html);
-			$('#span2').html(html2);
+	html2 += '</select>'
+		$('#span-combo-lingue').html(html);
+	$('#span2').html(html2);
 
 }
 
@@ -67,7 +67,7 @@ function getTesto(lang) {
 	var url = "getRatingQuestions.php?user="+getUsername()+"&token="+getToken()+"&lang="+lang;
 
 	console.debug("getTesto");
-	
+
 	var req = createXHTMLHttpRequest() ;
 	req.onreadystatechange = function(){
 		if (req.status == 200&req.readyState==4){
@@ -80,7 +80,7 @@ function getTesto(lang) {
 			return(false);
 		}
 	}
-	
+
 	req.open("GET", url, true);
 	req.send();
 }
@@ -90,14 +90,14 @@ function tryOut2() {
 	started = false;
 	outTime = false;
 	nowSeconds = 0;
-	
+
 	var e = document.getElementById("select-language");
 	var lan = e.options[e.selectedIndex].text;
-	
+
 
 	e = document.getElementById("select-language2");
 	var lan2 = e.options[e.selectedIndex].text;
-	
+
 	if (lan == lan2) {
 		alert("Impossible insert the same language. Please change it");
 		return;
@@ -105,7 +105,7 @@ function tryOut2() {
 	if (lan != null && lan != "-" && lan2 != null && lan2 != "-") {
 		var html = "";
 		html = 'Are you ready? <br /><a class="btn btn-default" href="javascript:void(0)" onClick="startTest()"> Go!</a>'
-		$("#skill-body").html(html);
+			$("#skill-body").html(html);
 		$('#skill-modal').modal('show');
 	}else{
 		alert("Please complete both fields");
@@ -117,7 +117,7 @@ function tryOut2() {
 function startTest() {
 	e = document.getElementById("select-language");
 	var lang = e.options[e.selectedIndex].text;
-	
+
 	getTesto(lang);
 	if (!started) {
 		started = true;
@@ -141,29 +141,29 @@ function showDomanda() {
 	var html = "";
 	var domanda = domande[0];
 	html += '<form>'
-	html += '<div class="panel panel-default"><div class="panel-heading"> <font size="2" >' + domanda.text_to_translate;
+		html += '<div class="panel panel-default"><div class="panel-heading"> <font size="2" >' + domanda.text_to_translate;
 	+'</font>  </div> </div>'
 	html += '<br/>'
 
-	html += '<br><div><center> <textarea rows="4" cols="110" id="textarea" placeholder="Please enter here the translation of the text here"></textarea></center>' + '</div>'
-	var selected = "";
+		html += '<br><div><center> <textarea rows="4" cols="110" id="textarea" placeholder="Please enter here the translation of the text here"></textarea></center>' + '</div>'
+		var selected = "";
 	var disabled = "";
 	if (outTime) {
 		disabled = "disabled"
 	}
 	html += '</form><br>'
-	html += '<div class="progress" id="bar-progress">'
-	html += '</div>'
-	if (started) {
-		html += '<span id="rimanente"></span>'
-	}
+		html += '<div class="progress" id="bar-progress">'
+			html += '</div>'
+				if (started) {
+					html += '<span id="rimanente"></span>'
+				}
 	if (outTime) {
 		html += '<div style="text-align:center;width:100%"><span id="scaduto" style="font-weight:bold;font-color:red>Tempo scaduto</span></div>'
 	}
 
 	html += '&nbsp;<a class="btn btn-default" href="javascript:void(0)" onclick="finishTest()"><i class="fa fa-paper-plane"></i>&nbsp;Submit</a>'
 
-	$("#skill-body").html(html);
+		$("#skill-body").html(html);
 	$("#skill-body").fadeIn("show");
 }
 
@@ -174,23 +174,23 @@ function finishTest() {
 		var traduzione = document.getElementById("textarea").value;
 		var e = document.getElementById("select-language");
 		var linguaF = e.options[e.selectedIndex].text;
-		
+
 		e = document.getElementById("select-language2");
 		var linguaT = e.options[e.selectedIndex].text;
 		var domanda = domande[0];
 		var testo = domanda.text_to_translate;
 
 		var temp = {
-			user : getUsername(),
-			token: getToken(),
-			testo : testo,
-			linguaF : linguaF,
-			linguaT : linguaT,
-			trad : traduzione	
+				user : getUsername(),
+				token: getToken(),
+				testo : testo,
+				linguaF : linguaF,
+				linguaT : linguaT,
+				trad : traduzione	
 		};
 		var stringPass = JSON.stringify(temp);
 		var data = stringPass
-		
+
 		$.ajax( {
 			type : "POST",
 			dataType : "application/json",
@@ -202,7 +202,7 @@ function finishTest() {
 				clearTimeout(myTimer);
 				nowSeconds = 0;
 				$('#skill-modal').modal('hide');
-				
+
 				$("#alertOK").html("Test Completed!");
 				$("#alertOK").fadeIn().delay(5000).fadeOut();
 			},
@@ -222,15 +222,77 @@ function getProgress() {
 	var html = "";
 
 	html += '<div class="progress-bar progress-bar-striped active" role="progressbar" aria-valuenow="'
-			+ nowSeconds
-			+ '" aria-valuemin="0" aria-valuemax="'
-			+ maxSeconds
-			+ '" style="width: '
-			+ parseInt((nowSeconds * 100) / maxSeconds)
-			+ '%">'
-	html += "</div>"
+		+ nowSeconds
+		+ '" aria-valuemin="0" aria-valuemax="'
+		+ maxSeconds
+		+ '" style="width: '
+		+ parseInt((nowSeconds * 100) / maxSeconds)
+		+ '%">'
+		html += "</div>"
 
-	$("#bar-progress").html(html);
+			$("#bar-progress").html(html);
 	$("#rimanente").html("Seconds remaining: " + (maxSeconds - nowSeconds))
 
+}
+
+
+
+
+
+function createTable(){
+
+	var url = "getRatingTest.php?user=" + getUsername()+"&token="+getToken();
+
+	var req = createXHTMLHttpRequest() ;
+	req.onreadystatechange = function(){
+		if (req.status == 200&req.readyState==4){
+			var data=JSON.parse(req.responseText);
+			gotData(data);
+			return(true);
+		}else{
+			mostraDialogTimed('errorPanel');
+			return(false);
+		}
+	}
+	req.open("GET",url,true);
+	req.send();
+
+
+	function gotData(data){
+		var html = "";
+		html += '<table style="width:100%" class="table">';
+		html += '<thead>';
+		html += '	<tr valign="middle">';
+		html += '		<th';
+		html += '			style="text-align:center;width:25%;height:32px;background-color:#EFEFEF">Language</th>';
+		html += '		<th';
+		html += '			style="text-align:center;width:25%;height:32px;background-color:#EFEFEF">Last Execution Date</th>';
+		html += '	</tr>';
+		html += '</thead>';
+		html += '<tbody>';
+		for ( var i = 0; i < data.length; i++) {
+			var classe="";
+			if (data[i].TotTest == null||data[i].TotTest <1) {
+				classe=' class="danger"'
+			}
+			html += '<tr valign="middle"  '+classe+'>'
+
+			var lang = data[i].Language
+			html += '<td style="text-align:center;width:25%;height:42px;border-top:1px solid #EFEFEF;border-bottom:1px solid #EFEFEF;border-left:1px solid #EFEFEF;border-right:1px solid #EFEFEF"> <b>'
+				+ lang
+				+ '</b> </td>'
+
+				var data1 = data[i].DataTest
+				data1 = data1.replace("T"," ")
+				data1 = data1.replace("Z","")
+				data1 = data1.substring(0,data1.length-3)
+				html += '<td style="text-align:center;width:25%;height:42px;border-top:1px solid #EFEFEF;border-bottom:1px solid #EFEFEF;border-left:1px solid #EFEFEF;border-right:1px solid #EFEFEF">' + data1 + '</td>'
+
+
+				html += '</tr>'
+					html += "</tbody>";
+			html += "</table>";
+			$('#tabella').html(html);
+		}
+	}
 }
