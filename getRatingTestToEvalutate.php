926769c99c1898333e093546c3ce03c8bee3faa9
Filippo Valle <filippo.valle@edu.unito.it>
2018-03-24 18:02:34 +0100

    avoid evaluate imself

--- getRatingTestToEvalutate.php
+++ getRatingTestToEvalutate.php
@@ -9,7 +9,7 @@ $langF=$_GET['langF'];
 
 if(!certToken($db, $user,$_GET['token'])) exit(json_encode(array("message"=>"wrong token", "statuscode"=>400)));
 
-$query = "select * from languagerating WHERE languagefrom='$langF';";
+$query = "select * from languagerating WHERE languagefrom='$langF' and username!='$user';";
 $result = $db->query($query);
 $row = $result->fetch(PDO::FETCH_ASSOC);
 $toExit=array("idtest"=>$row['idtest'], "LanguageF"=>$row['languagefrom'], "LanguageT"=>$row['languageto'], "DataTest"=>$row['datatest'], "TranslatedText"=>$row['translated']);
