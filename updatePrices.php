33f67d53d16f2c8d29f1e74a694a03f61a8ef413
Filippo Valle <filippo.valle@edu.unito.it>
2018-05-01 11:26:53 +0200

    round

--- updatePrices.php
+++ updatePrices.php
@@ -11,11 +11,11 @@ while($row = $result->fetch(PDO::FETCH_ASSOC)){
   $to=$row['to_l'];
   $from=$row['from_l'];
   $price=$row['price'];
-  $cur=substr($row['currency'],0,3);
+  $cur=substr($row['currency'],0,5);
   $query="SELECT currency, conversion from currencies where currency='$cur';";
   $convResult = $db->query($query);
   $convRow = $convResult->fetch(PDO::FETCH_ASSOC);
-  $priceEuro=round((float)$price/$convRow['conversion'],2);
+  $priceEuro=round((float)$price/$convRow['conversion'],5);
   $query="UPDATE language_pair SET price_euro='$priceEuro' WHERE username='$username' AND from_l='$from' AND to_l='$to' AND price='$price';";
   $db->query($query);
 }
