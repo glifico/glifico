1932bce4895b98ea6a75bd8f002bc22a2b180e4d
Filippo Valle <filippo.valle@edu.unito.it>
2018-05-02 07:42:21 +0200

    update prices every time a job is created

--- createJob.php
+++ createJob.php
@@ -1,40 +1,58 @@
 <?php
 include 'functions.php';
+include 'updatePrices.php';
 
-$db=getDB();
-if(!$db) exit;
+$db = getDB();
+if (! $db)
+    exit();
 
-if ($_SERVER['REQUEST_METHOD'] == 'POST')
-{
-  $data = json_decode(file_get_contents("php://input"),true);
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $data = json_decode(file_get_contents("php://input"), true);
 }
 
-if(!$data){
-  exit(json_encode(array("message"=>"wrong request","statuscode"=>500)));
+if (! $data) {
+    exit(json_encode(array(
+        "message" => "wrong request",
+        "statuscode" => 500
+    )));
 }
 
-$user=$data['user'];
-$url=$data['url'];
-$count=$data['count'];
-$selected=$data['translators'];
-$jobTitle=$data['job'];
-$jobDescr=$data['description'];
-$deadline=$data['deadline'];
-$translator=get_username($selected[0]['id']);
-$firstPrice=$selected[0]['total'];
-$firstCurrency=$selected[0]['currency'];
-$n_characters=$data['ncharacters'];
-$languagefrom=$data['languagefrom'];
+$user = $data['user'];
+$url = $data['url'];
+$count = $data['count'];
+$selected = $data['translators'];
+$jobTitle = $data['job'];
+$jobDescr = $data['description'];
+$deadline = $data['deadline'];
+$translator = get_username($selected[0]['id']);
+$firstPrice = $selected[0]['total'];
+$firstCurrency = $selected[0]['currency'];
+$n_characters = $data['ncharacters'];
+$languagefrom = $data['languagefrom'];
 
-if(!certTokenA($db, $user,$data['token'])) exit(json_encode(array("message"=>"wrong token", "statuscode"=>400)));
+if (! certTokenA($db, $user, $data['token']))
+    exit(json_encode(array(
+        "message" => "wrong token",
+        "statuscode" => 400
+    )));
 
-$query="INSERT INTO payments (job, description, status, username, document, languagefrom, ncharacters, translator, currency, deadline, price) VALUES ('$jobTitle','$jobDescr','To Be Assigned', '$user', '$url', '$languagefrom', '$n_characters', '$translator', '$firstCurrency','$deadline',$firstPrice);";
+$query = "INSERT INTO payments (job, description, status, username, document, languagefrom, ncharacters, translator, currency, deadline, price) VALUES ('$jobTitle','$jobDescr','To Be Assigned', '$user', '$url', '$languagefrom', '$n_characters', '$translator', '$firstCurrency','$deadline',$firstPrice);";
 $result = $db->query($query);
 
 $result->CloseCursor();
 
-//send_email([array("email"=>get_user_email($translator))],"There is a new job on glifico","You have a new job on glifico, got to https://glifico.com/pendingJobs.html to look it out!");
-send_email([array("email"=>"fvalle.glifico@outlook.com")],"There is a new job on glifico",get_user_email($translator));
+// send_email([array("email"=>get_user_email($translator))],"There is a new job on glifico","You have a new job on glifico, got to https://glifico.com/pendingJobs.html to look it out!");
+send_email([
+    array(
+        "email" => "fvalle.glifico@outlook.com"
+    )
+], "There is a new job on glifico", get_user_email($translator));
 
-exit(json_encode(array("message"=>"job created", "statuscode"=>200,"selected"=>json_encode($selected))));
+updateCurrencies();
+
+exit(json_encode(array(
+    "message" => "job created",
+    "statuscode" => 200,
+    "selected" => json_encode($selected)
+)));
 ?>
--- updatePrices.php
+++ updatePrices.php
@@ -1,27 +1,33 @@
 <?php
 include 'functions.php';
-$db = getDB();
-if (! $db)
-    exit();
-$query = "SELECT * from language_pair;";
-$result = $db->query($query);
 
-$toExit = [];
-while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
-    $username = $row['username'];
-    $to = $row['to_l'];
-    $from = $row['from_l'];
-    $price = $row['price'];
-    $cur = substr($row['currency'], 0, 3);
-    $query = "SELECT currency, conversion from currencies where currency='$cur';";
-    $convResult = $db->query($query);
-    $convRow = $convResult->fetch(PDO::FETCH_ASSOC);
-    $priceEuro = (float) $price / $convRow['conversion'];
-    $query = "UPDATE language_pair SET price_euro='$priceEuro' WHERE username='$username' AND from_l='$from' AND to_l='$to' AND price='$price';";
-    $db->query($query);
-    echo (json_encode(array("price euro"=>$priceEuro, "price" => $price, "conv" => $convRow['conversion'])));
+function updateCurrencies(){
+    $db = getDB();
+    if (! $db)
+        exit();
+        $query = "SELECT * from language_pair;";
+        $result = $db->query($query);
+        
+        $toExit = [];
+        while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
+            $username = $row['username'];
+            $to = $row['to_l'];
+            $from = $row['from_l'];
+            $price = $row['price'];
+            $cur = substr($row['currency'], 0, 3);
+            $query = "SELECT currency, conversion from currencies where currency='$cur';";
+            $convResult = $db->query($query);
+            $convRow = $convResult->fetch(PDO::FETCH_ASSOC);
+            $priceEuro = round((float) $price / $convRow['conversion'],6);
+            $query = "UPDATE language_pair SET price_euro='$priceEuro' WHERE username='$username' AND from_l='$from' AND to_l='$to' AND price='$price';";
+            $db->query($query);
+            echo (json_encode(array("price euro"=>$priceEuro, "price" => $price, "conv" => $convRow['conversion'])));
+        }
+        $result->CloseCursor();
 }
-$result->CloseCursor();
+
+updateCurrencies();
+
 exit(json_encode(array(
     "stauscode" => 200,
     "message" => "currencies updated"
