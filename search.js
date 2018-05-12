95d4a9512b947ba260f67de0ad06d91bdb122507
Filippo Valle <filippo.valle@edu.unito.it>
2018-05-12 18:22:47 +0200

    fix title

--- search.js
+++ search.js
@@ -271,10 +271,10 @@ angular.module("search",[]).controller("search",function($scope){
 
 	ctrl.createPopOverForUser= function(user){
 		var html='';
-		html='<a href="#"';
+		html='<a ';
 		//html+='<button type="button" class="btn btn-secondary"';
 		html+='data-container="body" data-toggle="popover" data-placement="right" data-trigger="focus" ';
-		html+='data-title=" '+user.FirstName+user.LastName+user.Id+'" ';
+		html+='title=" '+user.FirstName+user.LastName+user.Id+'" ';
 		html+='data-content=" ';
 		html+='mothertongue: '+user.Mothertongue+'</br>';
 		html+='" >';
