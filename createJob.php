d3b32a58ae6a2bc15bd4c83a93abbf4dce9b989f
Filippo Valle <filippo.valle@edu.unito.it>
2018-08-17 19:43:28 +0200

    remove debug

--- createJob.php
+++ createJob.php
@@ -20,15 +20,17 @@ if (! $data) {
 $user = $data['user'];
 $url = $data['url'];
 $count = $data['count'];
-if($count < 1) $count=1;
+if ($count < 1)
+    $count = 1;
 
 $selected = $data['translators'];
 $jobTitle = $data['job'];
-if(strlen($jobTitle) < 1) $jobTitle="Agency job";
+if (strlen($jobTitle) < 1)
+    $jobTitle = "Agency job";
 
 $jobDescr = $data['description'];
-if(strlen($jobDescr) < 1) $jobDescr="Job without desciprion";
-
+if (strlen($jobDescr) < 1)
+    $jobDescr = "Job without description";
 
 $deadline = $data['deadline'];
 $createdline = date('Y-m-d H:i:s');
@@ -39,9 +41,9 @@ if ($count > 0) {
     $secondtranslator = get_username($selected[1]['id']);
     $secondPrice = $selected[1]['total'];
     $secondCurrency = $selected[1]['currency'];
-}else{
+} else {
     $secondtranslator = "";
-    $secondPrice = -1;
+    $secondPrice = - 1;
     $secondCurrency = "-EUR";
 }
 $n_characters = $data['ncharacters'];
@@ -53,15 +55,13 @@ if (! certTokenA($db, $user, $data['token']))
         "statuscode" => 400
     )));
 
-    if($secondPrice = -1){
-$query = "INSERT INTO payments (job, description, status, username, document, languagefrom, ncharacters, translator, currency, deadline, price, whoaccepted, createdline) VALUES ('$jobTitle','$jobDescr','To Be Assigned', '$user', '$url', '$languagefrom', '$n_characters', '$translator', '$firstCurrency','$deadline','$firstPrice','0', '$createdline');";
-    }else{
-        $query = "INSERT INTO payments (job, description, status, username, document, languagefrom, ncharacters, translator, secondtranslator, secondstatus, currency, secondcurrency, deadline, price, secondprice, whoaccepted, createdline) VALUES ('$jobTitle','$jobDescr','To Be Assigned', '$user', '$url', '$languagefrom', '$n_characters', '$translator', '$secondtranslator', 'To Be Assigned', '$firstCurrency', '$secondCurrency' ,'$deadline','$firstPrice', '$secondPrice','0', '$createdline');";
-    }
+if ($secondPrice = - 1) {
+    $query = "INSERT INTO payments (job, description, status, username, document, languagefrom, ncharacters, translator, currency, deadline, price, whoaccepted, createdline) VALUES ('$jobTitle','$jobDescr','To Be Assigned', '$user', '$url', '$languagefrom', '$n_characters', '$translator', '$firstCurrency','$deadline','$firstPrice','0', '$createdline');";
+} else {
+    $query = "INSERT INTO payments (job, description, status, username, document, languagefrom, ncharacters, translator, secondtranslator, secondstatus, currency, secondcurrency, deadline, price, secondprice, whoaccepted, createdline) VALUES ('$jobTitle','$jobDescr','To Be Assigned', '$user', '$url', '$languagefrom', '$n_characters', '$translator', '$secondtranslator', 'To Be Assigned', '$firstCurrency', '$secondCurrency' ,'$deadline','$firstPrice', '$secondPrice','0', '$createdline');";
+}
 
-            $result = $db->query($query);
-            
-echo($query);
+$result = $db->query($query);
 $result->CloseCursor();
 
 // send_email([array("email"=>get_user_email($translator))],"There is a new job on glifico","You have a new job on glifico, go to https://glifico.com/pendingJobs.html to look it out!");
@@ -75,7 +75,7 @@ $retCurrencies = updateCurrencies();
 
 exit(json_encode(array(
     "message" => "job created",
-    "currencies"=> $retCurrencies,
+    "currencies" => $retCurrencies,
     "statuscode" => 200,
     "selected" => json_encode($selected)
 )));
