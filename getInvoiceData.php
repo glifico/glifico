6187f04db766530f5cd28ead13a1fc56d345c3ef
Filippo Valle <filippo.valle@edu.unito.it>
2018-08-22 18:03:19 +0200

    price

--- getInvoiceData.php
+++ getInvoiceData.php
@@ -15,10 +15,16 @@ $query="SELECT * FROM payments WHERE id='$id' and username='$user';";
 $result = $db->query($query);
 $row = $result->fetch(PDO::FETCH_ASSOC);
 
+if ($row['whoaccepted'] == 1) {
+    $price = (float) $row['price'];
+} else {
+    $price = (float) $row['secondprice'];
+}
+
 $query="SELECT * FROM invoices WHERE job='$id';";
 $result = $db->query($query);
 $invoice = $result->fetch(PDO::FETCH_ASSOC);
 
 $result->CloseCursor();
-exit (json_encode(array("id"=>$invoice['id'], "Price"=>$row['price'], "Description"=>$row['description'], "Job"=>$row['job'], "Date"=>$invoice['date'], "jobDate"=>$row['createdline'], "ncharacters"=> $row['ncharacters'], "languages"=>array("from"=>$row['languagefrom'],"to"=>$row['languageto']), "urgency"=>$row['urgency'])));
+exit (json_encode(array("id"=>$invoice['id'], "Price"=>$price, "Description"=>$row['description'], "Job"=>$row['job'], "Date"=>$invoice['date'], "jobDate"=>$row['createdline'], "ncharacters"=> $row['ncharacters'], "languages"=>array("from"=>$row['languagefrom'],"to"=>$row['languageto']), "urgency"=>$row['urgency'])));
 ?>
