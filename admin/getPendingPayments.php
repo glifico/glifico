26ff5f07815646a198f42e4b4c5b49b194b39e33
Filippo Valle <filippo.valle@zoho.com>
2018-09-02 14:52:42 +0200

    admin page

--- admin/getPendingPayments.php
+++ admin/getPendingPayments.php
@@ -3,39 +3,58 @@ include '../functions.php';
 
 $db = getDB();
 if (! $db)
-exit();
+    exit();
 
 $user = $_GET['user'];
 $token = $_GET['token'];
-if ($user!='admin' || $token !='glifico'){
-  exit(json_encode(array(
-    "message" => "wrong token",
-    "statuscode" => 400
-  )));
+if ($user != 'admin' || $token != 'glifico') {
+    exit(json_encode(array(
+        "message" => "wrong token",
+        "statuscode" => 400
+    )));
 }
 
-$query = "SELECT * FROM payments ORDER BY createdline DESC;";
+$query = "SELECT tp.*, i.id as invoice_number, i.date as invoice_date  FROM payments tp LEFT JOIN invoices i on tp.id = i.job ORDER BY tp.createdline DESC;";
 $result = $db->query($query);
 $toExit = [];
 while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
-  array_push($toExit, array(
-    "id" => $row['id'],
-    "job" => $row['job'],
-    "price" => $row['price'],
-    "currency" => $row['currency'],
-    "status" => $row['status'],
-    "document" => $row['preview'],
-    "description" => $row['description'],
-    "deadline" => $row['deadline'],
-    "createdline" => $row['createdline'],
-    "ncharacters" => $row['ncharacters'],
-    "translated" => $row['translated'],
-    "choice"=>$row['whoaccepted'],
-    "price" => [$row['price'], $row['secondprice']],
-    "currency" => [$row['currency'],$row['secondcurrency']],
-  ));
-}
+    if ($row['whoaccepted'] <= 1) {
+        $translator=$row['translator'];
+        $price = $row['price'];
+        $currency = $row['currency'];
+        $status = $row['status'];
+        $translator_price = getLanguagePrice($row['translator'], $row['languagefrom'], $row['languageto'], $row['field']);
+    } else {
+        $translator=$row['secondtranslator'];
+        $price = $row['secondprice'];
+        $currency = $row['secondcurrency'];
+        $status = $row['secondstatus'];
+        $translator_price = getLanguagePrice($row['secondtranslator'], $row['languagefrom'], $row['languageto'], $row['field']);
+    }
 
+    array_push($toExit, array(
+        "id" => $row['id'],
+        "job" => $row['job'],
+        "price" => $row['price'],
+        "currency" => $row['currency'],
+        "field" => $row['field'],
+        "status" => $row['status'],
+        "preview" => $row['preview'],
+        "document" => $row['document'], 
+        "translated" => $row['translated'],
+        "description" => $row['description'],
+        "price" => $price,
+        "currency" => $currency,
+        "deadline" => $row['deadline'],
+        "createdline" => $row['createdline'],
+        "ncharacters" => $row['ncharacters'],
+        "choice" => $row['whoaccepted'],
+        "translator"=>$translator,
+        "status" => $status,
+        "urgency"=>$row['urgency'],
+        "price_to_translator" => $translator_price * $row['ncharacters']
+    ));
+}
 
 $result->CloseCursor();
 
--- admin/payments.js
+++ admin/payments.js
@@ -13,28 +13,19 @@ angular.module("Payments",[]).controller("Payments",function(){
 		return "";
 	}
 
-	ctrl.getToolTip=function(doc){
-		if (doc.status=="To Be Assigned") return "Translators need to Accept";
-		if (doc.status=="Assigned") return "Translator is working";
-		if (doc.status=="Refused") return "Translator refused";
-		if (doc.status=="Translated") return "Work finished";
-		if (doc.status=="Accepted") return "Work to be paid";
-		if (doc.status=="Not Accepted") return "work neither accepted or paid";
-		if (doc.status=="Paid") return "ok";
-		if (doc.status=="Completed") return "Work closed definetely";
-		return "";
-	}
 
 	ctrl.createTable=function(){
 		var html="";
 		html+='<table id="d_table" class="table table-responsive">';
 		html+='<thead class="thead-default">';
 		html+='<tr class="row">';
-		html+='<th class="col-md-2" style="text-align:center;">Job</th>';
-		html+='<th class="col-md-2" style="text-align:center;">Price</th>';
-		html+='<th class="col-md-2" style="text-align:center;"># characters</th>';
-		html+='<th class="col-md-2" style="text-align:center;">Creation date</th>';
-		html+='<th class="col-md-2" style="text-align:center;">Deadline</th>';
+		html+='<th class="col-md-1" style="text-align:center;">Id</th>';
+		html+='<th class="col-md-1" style="text-align:center;">Job</th>';
+		html+='<th class="col-md-2" style="text-align:center;">Price paid by agency</th>';
+		html+='<th class="col-md-1" style="text-align:center;"># characters</th>';
+		html+='<th class="col-md-1" style="text-align:center;">Creation date</th>';
+		html+='<th class="col-md-1" style="text-align:center;">Deadline</th>';
+		html+='<th class="col-md-2" style="text-align:center;">Price to be paied to translator</th>';
 		html+='<th class="col-md-2" style="text-align:center;">Status</th>';
 		html+='<th class="col-md-2" style="text-align:center;"></th>';
 		html+='</tr>';
@@ -43,60 +34,37 @@ angular.module("Payments",[]).controller("Payments",function(){
 		for (var i = 0; i < ctrl.documents.length; i++) {
 			var doc=ctrl.documents[i];
 			html+='<tr class="row '+ctrl.getClass(doc)+'">';
-			html+='<td class="col-md-2">'+doc.job+'</td>';
-			if(doc.choice==0){
-				html+='<td class="col-md-2">';
-				html+=doc.price[0]+" "+doc.currency[0]+"<br>";
-				html+=doc.price[1]+" "+doc.currency[1];
-				html+='</td>';
-			}else{
-				html+='<td class="col-md-2">'+doc.price+" "+doc.currency+'</td>';
-			}
-			html+='<td class="col-md-2">'+doc.ncharacters+'</td>';
-			html+='<td class="col-md-2">'+doc.createdline+'</td>';
-			html+='<td class="col-md-2">'+doc.deadline+'</td>';
-			html+='<td class="col-md-2" data-toggle="tooltip" data-placement="top" title="'+ctrl.getToolTip(doc)+'">'+doc.status;
-			if(doc.status=="Paid"){
+			html+='<td class="col-md-1">'+doc.id+'</td>';
+			html+='<td class="col-md-1">'+doc.job+'</td>';
+			html+='<td class="col-md-2">'+doc.price+" "+doc.currency+'</td>';
+			html+='<td class="col-md-1">'+doc.ncharacters+'</td>';
+			html+='<td class="col-md-1">'+doc.createdline+'</td>';
+			html+='<td class="col-md-1">'+doc.deadline+'</td>';
+			html+='<td class="col-md-2">'+doc.price_to_translator+'</td>';
+			html+='<td class="col-md-2">'+doc.status;
+			if(doc.status=="Closed"){
 				html+=' <i class="fas fa-check" aria-hidden="true"></i>';
 			}
 			html+='</td>';
 			html+='<td class="col-md-2">';
 			html+='<div id="'+doc.id+'">';
-			if(doc.status=="Closed"){
-
-			}else if(doc.status=="Paid"){
-				html+='<button type="button" class="btn btn-success" data-toggle="modal" data-target="#jobModal"';
-				html+='data-status="'+doc.status+'"';
-				html+='data-job="'+doc.job+'"';
-				html+='data-id="'+doc.id+'"';
-				html+=' data-price="'+doc.price+'"';
-				html+=' data-createdline="'+doc.createdline+'"';
-				html+=' data-deadline="'+doc.deadline+'"';
-				html+=' data-currency="'+doc.currency+'"';
-				html+=' data-description="'+doc.description+'"';
-				html+=' data-document="'+doc.document+'"';
-				html+='>Download recipe</button>';
-			}else if (doc.status=="Approved"){
-				html+='<button onClick="newPayment('+doc.id+')"  class="btn btn-primary">Pay now!</button>';
-			}else if(doc.status=="Assigned"){
-				html+='<button type="button" class="btn btn-info" data-toggle="modal" data-target="#jobModal"';
-				html+='data-job="'+doc.job+'"';
-				html+=' data-price="'+doc.price+'"';
-				html+=' data-currency="'+doc.currency+'"';
-				html+=' data-description="'+doc.description+'"';
-				html+='>Show job</button>';
-			}else if(doc.status=="Translated"){
-				html+='<button type="button" class="btn btn-warning" data-toggle="modal" data-target="#jobModal"';
-				html+='data-status="'+doc.status+'"';
-				html+='data-job="'+doc.job+'"';
-				html+='data-id="'+doc.id+'"';
-				html+=' data-price="'+doc.price+'"';
-				html+=' data-currency="'+doc.currency+'"';
-				html+=' data-description="'+doc.description+'"';
-				html+=' data-document="'+doc.document+'"';
-				html+='>Show Job</button>';
-			}else if(doc.status=="To Be Assigned"){
-			}
+			html+='<button type="button" class="btn btn-info" data-toggle="modal" data-target="#jobModal"';
+			html+='data-status="'+doc.status+'"';
+			html+=' data-job="'+doc.job+'"';
+			html+=' data-id="'+doc.id+'"';
+			html+=' data-price="'+doc.price+'"';
+			html+=' data-price_to_translator="'+doc.price_to_translator+'"';
+			html+=' data-status="'+doc.status+'"';
+			html+=' data-field="'+doc.field+'"';
+			html+=' data-createdline="'+doc.createdline+'"';
+			html+=' data-deadline="'+doc.deadline+'"';
+			html+=' data-currency="'+doc.currency+'"';
+			html+=' data-description="'+doc.description+'"';
+			html+=' data-document="'+doc.document+'"';
+			html+=' data-preview="'+doc.preview+'"';
+			html+=' data-translated="'+doc.translated+'"';
+			html+=' data-urgency="'+doc.urgency+'"';
+			html+='>Show more</button>';
 			html+='</div>';
 			html+='</td>';
 			html+='</tr>';
