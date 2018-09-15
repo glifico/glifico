06fd58d3fc82a166bca939276217973ce87c2b3f
Filippo Valle <filippo.valle@zoho.com>
2018-09-15 22:32:27 +0200

    add address

--- app/glificoFooter.component.js
+++ app/glificoFooter.component.js
@@ -2,7 +2,8 @@ angular.module("glificoFooter",[])
 .component("glificofooter",{
 	controller: function(){
 		var ctrl=this;
-		ctrl.address="...";
+		ctrl.address="10100 Torino (TO)"
+		ctrl.piva="03692530045";
 	},
 	templateUrl: "app/glificoFooter.template.html"
 })
