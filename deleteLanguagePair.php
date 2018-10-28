7e7f22812929a4d5883ddb41d7cfbbf00eebd48a
Filippo Valle <filippo.valle@zoho.com>
2018-10-28 19:10:39 +0100

    delete only field

--- deleteLanguagePair.php
+++ deleteLanguagePair.php
@@ -24,12 +24,27 @@ $field=$pair['Field'];
 
 if(!certToken($db, $user, $data['token'])) exit(json_encode(array("message"=>"wrong token", "statuscode"=>400)));
 
+$query="select id, job  from payments where translator ='$user' and languagefrom = '$from' and field = 'select all' and languageto = '$to' and whoaccepted=1;";
+$result = $db->query($query);
+$row = $result->fetch(PDO::FETCH_ASSOC);
+if (strlen(htmlspecialchars($row["job"])) > 1) {
+    exit (json_encode(array("message"=>"job in progress", "job" =>$row, "statuscode"=>301)));
+}
+
 $query="select id, job  from payments where translator ='$user' and languagefrom = '$from' and field = '$field' and languageto = '$to' and whoaccepted=1;";
 $result = $db->query($query);
 $row = $result->fetch(PDO::FETCH_ASSOC);
 if (strlen(htmlspecialchars($row["job"])) > 1) {
     exit (json_encode(array("message"=>"job in progress", "job" =>$row, "statuscode"=>301)));
 }
+
+$query="select id, job  from payments where secondtranslator ='$user' and languagefrom = '$from' and languageto = '$to' and field = 'select all' and whoaccepted=2;";
+$result = $db->query($query);
+$row = $result->fetch(PDO::FETCH_ASSOC);
+if (strlen(htmlspecialchars($row["job"])) > 1) {
+    exit (json_encode(array("message"=>"job in progress", "job" =>$row, "statuscode"=>302)));
+}
+
 
 $query="select id, job  from payments where secondtranslator ='$user' and languagefrom = '$from' and languageto = '$to' and field = '$field' and whoaccepted=2;";
 $result = $db->query($query);
@@ -38,7 +53,8 @@ if (strlen(htmlspecialchars($row["job"])) > 1) {
     exit (json_encode(array("message"=>"job in progress", "job" =>$row, "statuscode"=>302)));
 }
 
-$query="DELETE FROM language_pair WHERE username='$user' AND from_l='$from' AND to_l='$to';";
+
+$query="DELETE FROM language_pair WHERE username='$user' AND from_l='$from' AND to_l='$to' and field='$field';";
 $result = $db->query($query);
 
 user_try_del_lang($user,$from);
