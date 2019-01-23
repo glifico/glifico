6a891bb48234b165915b03e96999f74e35c4988e
Filippo Valle <filippo.valle@edu.unito.it>
2019-01-23 08:00:41 +0100

    add per word price in search

--- search.js
+++ search.js
@@ -459,6 +459,7 @@ angular.module("search",[]).controller("search",function($scope){
 	}
 
 	ctrl.calculateFeasibility=function(){
+		ctrl.TrCharacters = ctrl.TrWords * 6;
 		var timeDiff = ctrl.TrDeadline.getTime() - ctrl.today.getTime();
 		var days = Math.ceil(timeDiff / (1000 * 3600 * 24));
 
