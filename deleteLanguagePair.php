fa8d960a4233a04e9508822ae1d606f70418b80e
Filippo Valle <filippo.valle@edu.unito.it>
2019-02-14 09:26:55 +0100

    Implement Price per word for translators

--- deleteLanguagePair.php
+++ deleteLanguagePair.php
@@ -3,62 +3,85 @@ require_once 'functions.php';
 require_once 'languages.php';
 require_once 'speak.php';
 
+$db = getDB();
+if (! $db)
+    exit();
 
-$db=getDB();
-if(!$db) exit;
-
-if ($_SERVER['REQUEST_METHOD'] == 'POST')
-{
-    $data = json_decode(file_get_contents("php://input"),true);
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $data = json_decode(file_get_contents("php://input"), true);
 }
 
-if(!$data){
-    exit(json_encode(array("message"=>"wrong request","statuscode"=>500)));
+if (! $data) {
+    exit(json_encode(array(
+        "message" => "wrong request",
+        "statuscode" => 500
+    )));
 }
 
-$user=$data['user'];
-$pair=$data['values'];
-$from=$pair['LanguageFrom'];
-$to=$pair['LanguageTo'];
-$field=$pair['Field'];
+$user = $data['user'];
+$pair = $data['values'];
+$from = $pair['LanguageFrom'];
+$to = $pair['LanguageTo'];
+$field = $pair['Field'];
 
-if(!certToken($db, $user, $data['token'])) exit(json_encode(array("message"=>"wrong token", "statuscode"=>400)));
+if (! certToken($db, $user, $data['token']))
+    exit(json_encode(array(
+        "message" => "wrong token",
+        "statuscode" => 400
+    )));
 
-$query="select id, job  from payments where translator ='$user' and languagefrom = '$from' and field = 'select all' and languageto = '$to' and whoaccepted=1 and status!='Closed';";
+$query = "select id, job  from payments where translator ='$user' and languagefrom = '$from' and field = 'select all' and languageto = '$to' and whoaccepted=1 and status!='Closed';";
 $result = $db->query($query);
 $row = $result->fetch(PDO::FETCH_ASSOC);
 if (strlen(htmlspecialchars($row["job"])) > 1) {
-    exit (json_encode(array("message"=>"job in progress", "job" =>$row, "statuscode"=>301)));
+    exit(json_encode(array(
+        "message" => "job in progress",
+        "job" => $row,
+        "statuscode" => 301
+    )));
 }
 
-$query="select id, job  from payments where translator ='$user' and languagefrom = '$from' and field = '$field' and languageto = '$to' and whoaccepted=1 and status!='Closed';";
+$query = "select id, job  from payments where translator ='$user' and languagefrom = '$from' and field = '$field' and languageto = '$to' and whoaccepted=1 and status!='Closed';";
 $result = $db->query($query);
 $row = $result->fetch(PDO::FETCH_ASSOC);
 if (strlen(htmlspecialchars($row["job"])) > 1) {
-    exit (json_encode(array("message"=>"job in progress", "job" =>$row, "statuscode"=>301)));
+    exit(json_encode(array(
+        "message" => "job in progress",
+        "job" => $row,
+        "statuscode" => 301
+    )));
 }
 
-$query="select id, job  from payments where secondtranslator ='$user' and languagefrom = '$from' and languageto = '$to' and field = 'select all' and whoaccepted=2 and secondstatus!='Closed';";
+$query = "select id, job  from payments where secondtranslator ='$user' and languagefrom = '$from' and languageto = '$to' and field = 'select all' and whoaccepted=2 and secondstatus!='Closed';";
 $result = $db->query($query);
 $row = $result->fetch(PDO::FETCH_ASSOC);
 if (strlen(htmlspecialchars($row["job"])) > 1) {
-    exit (json_encode(array("message"=>"job in progress", "job" =>$row, "statuscode"=>302)));
+    exit(json_encode(array(
+        "message" => "job in progress",
+        "job" => $row,
+        "statuscode" => 302
+    )));
 }
 
-
-$query="select id, job  from payments where secondtranslator ='$user' and languagefrom = '$from' and languageto = '$to' and field = '$field' and whoaccepted=2 and secondstatus!='Closed';";
+$query = "select id, job  from payments where secondtranslator ='$user' and languagefrom = '$from' and languageto = '$to' and field = '$field' and whoaccepted=2 and secondstatus!='Closed';";
 $result = $db->query($query);
 $row = $result->fetch(PDO::FETCH_ASSOC);
 if (strlen(htmlspecialchars($row["job"])) > 1) {
-    exit (json_encode(array("message"=>"job in progress", "job" =>$row, "statuscode"=>302)));
+    exit(json_encode(array(
+        "message" => "job in progress",
+        "job" => $row,
+        "statuscode" => 302
+    )));
 }
 
-
-$query="DELETE FROM language_pair WHERE username='$user' AND from_l='$from' AND to_l='$to' and field='$field';";
+$query = "DELETE FROM language_pair WHERE username='$user' AND from_l='$from' AND to_l='$to' and field='$field';";
 $result = $db->query($query);
 
-user_try_del_lang($user,$from);
-user_try_del_lang($user,$to);
+user_try_del_lang($user, $from);
+user_try_del_lang($user, $to);
 
-exit (json_encode(array("message"=>"language deleted", "statuscode"=>200)));
+exit(json_encode(array(
+    "message" => "language deleted",
+    "statuscode" => 200
+)));
 ?>
--- getLanguagePairsData.php
+++ getLanguagePairsData.php
@@ -1,19 +1,32 @@
 <?php
 include 'functions.php';
 
-$db=getDB();
-if(!$db) exit;
+$db = getDB();
+if (! $db)
+    exit();
 
-$user=$_GET['user'];
-if(!certToken($db, $user, $_GET['token'])) exit(json_encode(array("message"=>"wrong token", "statuscode"=>400)));
+$user = $_GET['user'];
+if (! certToken($db, $user, $_GET['token']))
+    exit(json_encode(array(
+        "message" => "wrong token",
+        "statuscode" => 400
+    )));
 
-$query="SELECT username, from_l, to_l, price, currency, field, service from language_pair WHERE username='$user';";
+$query = "SELECT username, from_l, to_l, price, currency, field, service from language_pair WHERE username='$user';";
 $result = $db->query($query);
 
-$toExit=[];
-while($row = $result->fetch(PDO::FETCH_ASSOC)){
-  array_push($toExit,array("LanguageFrom"=>$row['from_l'], "LanguageTo"=>$row['to_l'], "Price"=>$row['price'], "Currency"=>$row['currency'],"Field"=>$row['field'],"Service"=>$row['service']));
+$toExit = [];
+while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
+    $price = round($row['price']* 6., 4);
+    array_push($toExit, array(
+        "LanguageFrom" => $row['from_l'],
+        "LanguageTo" => $row['to_l'],
+        "Price" => $price,
+        "Currency" => $row['currency'],
+        "Field" => $row['field'],
+        "Service" => $row['service']
+    ));
 }
 
-exit (json_encode($toExit));
+exit(json_encode($toExit));
 ?>
